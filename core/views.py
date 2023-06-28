06a56f69ab81f33276d60fb239f852283464e657
Serdar SAYIN <serdarsayin61@gmail.com>
2023-06-28 23:12:30 +0300

    refactor: create layout view function

--- core/views.py
+++ core/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from core.models import *
+
+
 # Create your views here.
 
-def index(request):
+def layout(request):
     site_title = GeneralSetting.objects.get(name='site_title').parameter
     site_keywords = GeneralSetting.objects.get(name='site_keywords').parameter
     site_description = GeneralSetting.objects.get(name='site_description').parameter
@@ -12,11 +14,39 @@ def index(request):
     about_myself_welcome = GeneralSetting.objects.get(name='about_myself_welcome').parameter
     about_myself_footer = GeneralSetting.objects.get(name='about_myself_footer').parameter
 
-    #Images
+    # Images
     header_logo = ImageSetting.objects.get(name='header_logo').file
     home_banner_image = ImageSetting.objects.get(name='home_banner_image').file
     site_favicon = ImageSetting.objects.get(name='site_favicon').file
 
+    # Social Media
+    social_medias = SocialMedia.objects.all()
+
+    # Document
+    documents = Document.objects.all()
+
+    context = {
+        'site_title': site_title,
+        'site_keywords': site_keywords,
+        'site_description': site_description,
+        'home_banner_name': home_banner_name,
+        'home_banner_title': home_banner_title,
+        'home_banner_description': home_banner_description,
+        'about_myself_welcome': about_myself_welcome,
+        'about_myself_footer': about_myself_footer,
+        # images
+        'header_logo': header_logo,
+        'home_banner_image': home_banner_image,
+        'site_favicon': site_favicon,
+        # social_medias
+        'social_medias': social_medias,
+        # documents
+        'documents': documents,
+    }
+    return context
+
+
+def index(request):
     # Skills
     skills = Skill.objects.all()
 
@@ -25,43 +55,20 @@ def index(request):
     # Education
     educations = Education.objects.all().order_by('-start_date')
 
-    # Social Media
-    social_medias = SocialMedia.objects.all()
-
-    # Document
-    documents = Document.objects.all()
-
-    context ={
-        'site_title':site_title,
-        'site_keywords': site_keywords,
-        'site_description': site_description,
-        'home_banner_name': home_banner_name,
-        'home_banner_title': home_banner_title,
-        'home_banner_description': home_banner_description,
-        'about_myself_welcome': about_myself_welcome,
-        'about_myself_footer': about_myself_footer,
-        #images
-        'header_logo': header_logo,
-        'home_banner_image': home_banner_image,
-        'site_favicon': site_favicon,
-        #skills
+    context = {
+        # skills
         'skills': skills,
 
-        #experiences
+        # experiences
         'experiences': experiences,
 
-        #educations
+        # educations
         'educations': educations,
 
-        #social_medias
-        'social_medias': social_medias,
-
-        #documents
-        'documents': documents,
     }
-    return render(request, 'index.html',context=context)
+    return render(request, 'index.html', context=context)
 
 
-def redirect_urls(request,slug):
+def redirect_urls(request, slug):
     doc = get_object_or_404(Document, slug=slug)
     return redirect(doc.file.url)
