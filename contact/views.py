f47ebeac036f6a2b5ff9371ba4c5d946cb16e5a0
Serdar SAYIN <serdarsayin61@gmail.com>
2023-06-29 22:24:14 +0300

    feat: create message model

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from contact.models import Message
 
 
 # Create your views here.
@@ -7,9 +8,28 @@ from django.http import JsonResponse
 def contact(request):
     return render(request, 'contact.html')
 
+
 def contact_form(request):
+    if request.POST:
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        subject = request.POST.get('subject')
+
+        Message.objects.create(
+            name=name,
+            email=email,
+            message=message,
+            subject=subject
+        )
+
+        success = True
+        message = 'Contact form sent successfully.'
+    else:
+        success = False
+        message = 'Request method is not a valid'
     context = {
-        'success': True,
-        'message': 'Contact form sent successfully.'
+        'success': success,
+        'message': message,
     }
     return JsonResponse(context)
