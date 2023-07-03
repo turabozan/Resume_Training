e703fc6d7ee3e05dd1e94bcf42cb76983f81bfbf
Serdar SAYIN <serdarsayin61@gmail.com>
2023-07-03 15:51:57 +0300

    feat: create form and send mail

--- contact/views.py
+++ contact/views.py
@@ -1,30 +1,42 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from contact.models import Message
+from contact.forms import ContactForm
 
 
 # Create your views here.
 
 def contact(request):
-    return render(request, 'contact.html')
+    contact_form = ContactForm()
+    context = {
+        'contact_form': contact_form,
+    }
+    return render(request, 'contact.html', context=context)
 
 
 def contact_form(request):
     if request.POST:
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-        subject = request.POST.get('subject')
+        contact_form = ContactForm(request.POST or None)
+        if contact_form.is_valid():
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            message = request.POST.get('message')
+            subject = request.POST.get('subject')
 
-        Message.objects.create(
-            name=name,
-            email=email,
-            message=message,
-            subject=subject
-        )
+            Message.objects.create(
+                name=name,
+                email=email,
+                message=message,
+                subject=subject
+            )
+            # mail to send
+            contact_form.send_email()
 
-        success = True
-        message = 'Contact form sent successfully.'
+            success = True
+            message = 'Contact form sent successfully.'
+        else:
+            success = False
+            message = 'Request method is not a valid'
     else:
         success = False
         message = 'Request method is not a valid'
--- resume/settings.py
+++ resume/settings.py
@@ -138,3 +138,13 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'serdarsayin61@gmail.com'
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = 'Serdar SAYIN <serdarsayin61@gmail.com>'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
